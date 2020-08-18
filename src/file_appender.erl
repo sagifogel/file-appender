18c2bb5d0ce8c6d540a36697daa9db0ae6a6b3fe
sagifogel <sagi.fogel@gmail.com>
2020-08-18 15:08:30 +0300

    support for loading different configuration

--- src/file_appender.erl
+++ src/file_appender.erl
@@ -1,30 +1,30 @@
 -module(file_appender).
 -import(string, [concat/2, to_integer/1, right/2]).
 -import(lists, [keysearch/3]).
--export([start/1, appender/3, append_line/2]).
+-export([start/1, appender/3, append_line/2, read_conf/1]).
 -record(config, {termination_interval = 0}).
 
 start(FilePath) ->
-  Conf = read_conf(),
+  Conf = read_conf("app.config"),
   case file:open(FilePath, [append]) of
     {ok, Fn} -> spawn(fun() -> appender(FilePath, Fn, Conf) end);
     {error, err_value} -> io:fwrite("could not open file ~p. ~p", [FilePath, err_value])
   end.
 
-read_conf() ->
+read_conf(FileName) ->
   {ok, CurrentDir} = file:get_cwd(),
-  Conf_path = concat(CurrentDir, "/resources/app.config"),
+  Conf_path = CurrentDir ++ "/resources/" ++ FileName,
   case file:consult(Conf_path) of
     {ok, Conf} ->
       {_, {_, Value}} = keysearch(termination_interval_ms, 1, Conf),
       Interval = case to_integer(Value) of
-                   {error, no_integer} -> 10000;
+                   {error, no_integer} -> 5000;
                    {ParsedInterval, _} -> ParsedInterval
                  end,
       #config{termination_interval = Interval};
     {error, _} ->
       io:fwrite("could not find configuration file, using default configuration"),
-      #config{termination_interval = 10000}
+      #config{termination_interval = 5000}
   end.
 
 append_line(Pid, Line) ->
